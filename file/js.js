9c2fe729218765b08ff2c6d2fdcad0253f77ab93
Awedk_Air_ <101848961+Awedk-Air@users.noreply.github.com>
2023-07-11 00:16:51 +0800

    Update js.js

--- file/js.js
+++ file/js.js
@@ -23,33 +23,36 @@ document.onclick = function(){
     y = 30;
     setTimeout("y = 80;", 300)
 }
-var a = setInterval(function(){
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(0,0,document.querySelector("#zhengbindiv").clientWidth,document.querySelector("#zhengbindiv").height);
-    if (!doing){
-        if (Math.random() < 0.5){
-            doing = true;
-            x = 200;
-        }
-    } else {
-        if (x > -60){
-            ctx.drawImage(shaoxiang, x, 85, 30, 50)
-            x -= speed;
-            speed += 0.01
-            if (x >= 5 && x <= 70) {
-                if (y == 30){
-                    score += 1;
-                } else if (x >= 20 && x <= 40) {
-                    ctx.fillStyle = "#ffffff";
-                    ctx.fillRect(0,0,document.querySelector("#zhengbindiv").clientWidth,document.querySelector("#zhengbindiv").height);
-                    document.querySelector("h1").innerHTML = "⭐️游戏结束⭐️"
-                    clearInterval(a);
-                }
+setTimeout(function(){
+    var a = setInterval(function(){
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(0,0,document.querySelector("#zhengbindiv").clientWidth,document.querySelector("#zhengbindiv").height);
+        if (!doing){
+            if (Math.random() < 0.5){
+                doing = true;
+                x = 200;
             }
         } else {
-            doing = false;
+            if (x > -60){
+                ctx.drawImage(shaoxiang, x, 85, 30, 50)
+                x -= speed;
+                speed += 0.01
+                if (x >= 5 && x <= 70) {
+                    if (y == 30){
+                        score += 1;
+                    } else if (x >= 20 && x <= 40) {
+                        ctx.fillStyle = "#ffffff";
+                        ctx.fillRect(0,0,document.querySelector("#zhengbindiv").clientWidth,document.querySelector("#zhengbindiv").height);
+                        document.querySelector("h1").innerHTML = "⭐️游戏结束⭐️"
+                        clearInterval(a);
+                    }
+                }
+            } else {
+                doing = false;
+            }
         }
-    }
-    ctx.drawImage(zhengbin, 10, y, 60, 60);
-    document.querySelector("#gd").innerHTML = "分数："+score
-}, 50);
+        ctx.drawImage(zhengbin, 10, y, 60, 60);
+        document.querySelector("#gd").innerHTML = "分数："+score
+    }, 50);
+}, 3000)
+
