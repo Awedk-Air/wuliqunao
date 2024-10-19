c7ed5099756075eef132e051ec86e29974b80fc2
cyn <101848961+Awedk-Air@users.noreply.github.com>
2024-10-19 14:58:18 +0800

    Update js2.js

--- file/js2.js
+++ file/js2.js
@@ -1,19 +1,19 @@
 var number = 1;
 var gongde = 0;
-var zhengbin = new Image();
-var shaoxiang = new Image();
+var wuwei = new Image();
+var foreaver = new Image();
 var x = 200;
 var y = 80;
 var count = 0;
 var doing = false;
 var speed = 10;
 var score = 0;
-var ctx = document.querySelector("#zhengbindiv").getContext("2d");
+var ctx = document.querySelector("#wuweidiv").getContext("2d");
 var s = false;
-zhengbin.src = "./img/未命名33.png";
-shaoxiang.src = "./img/烧香.png";
-zhengbin.onload = function() {
-    ctx.drawImage(zhengbin, 10, y,  60, 60)
+wuwei.src = "./img/未命名33.png";
+foreaver.src = "./img/未命名44.png";
+wuwei.onload = function() {
+    ctx.drawImage(wuwei, 10, y,  60, 60)
 }
 document.onclick = function(){
     if (!s){
@@ -28,15 +28,15 @@ document.onclick = function(){
         doing = false;
         speed = 10;
         score = 0;
-        document.querySelector("h1").innerHTML = "⭐️帮助吴魏跳过烧香⭐️";
+        document.querySelector("h1").innerHTML = "⭐️帮助吴魏跳过李永远⭐️";
     }
 }
 ctx.fillStyle = "#ffffff";
-ctx.fillRect(0,0,document.querySelector("#zhengbindiv").clientWidth,document.querySelector("#zhengbindiv").height);
+ctx.fillRect(0,0,document.querySelector("#wuweidiv").clientWidth,document.querySelector("#wuweidiv").height);
 function main(){
     var a = setInterval(function(){
         ctx.fillStyle = "#ffffff";
-        ctx.fillRect(0,0,document.querySelector("#zhengbindiv").clientWidth,document.querySelector("#zhengbindiv").height);
+        ctx.fillRect(0,0,document.querySelector("#wuweidiv").clientWidth,document.querySelector("#wuweidiv").height);
         if (!doing){
             if (Math.random() < 0.5){
                 doing = true;
@@ -44,7 +44,7 @@ function main(){
             }
         } else {
             if (x > -60){
-                ctx.drawImage(shaoxiang, x, 85, 30, 50)
+                ctx.drawImage(foreaver, x, 85, 30, 50)
                 x -= speed;
                 speed += 0.01
                 if (x >= 5 && x <= 70) {
@@ -52,7 +52,7 @@ function main(){
                         score += 1;
                     } else if (x >= 20 && x <= 40) {
                         ctx.fillStyle = "#ffffff";
-                        ctx.fillRect(0,0,document.querySelector("#zhengbindiv").clientWidth,document.querySelector("#zhengbindiv").height);
+                        ctx.fillRect(0,0,document.querySelector("#wuweidiv").clientWidth,document.querySelector("#wuweidiv").height);
                         document.querySelector("h1").innerHTML = "⭐️游戏结束，点击屏幕重来⭐️"
                         setTimeout("s = true;", 1000)
                         clearInterval(a);
@@ -62,7 +62,7 @@ function main(){
                 doing = false;
             }
         }
-        ctx.drawImage(zhengbin, 10, y, 60, 60);
+        ctx.drawImage(wuwei, 10, y, 60, 60);
         document.querySelector("#gd").innerHTML = "分数："+score
     }, 50);
 }
